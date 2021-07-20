a36dd6e3faa211f9b03668e27526b475e05695e7
Abimbola Ojikutu <aojikutu20@aiip.alueducation.com>
2021-07-20 07:27:44 +0100

    Added testcode to readme as an example

--- testcode.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-url = 'http://127.0.0.1:5000/results'
-resp = requests.post(url, data=json.dumps({"inputs":[[0.00632,18.0,2.31,0.0,0.538,6.575,65.2,4.0900,1.0,296.0,15.3,396.90,4.98]]}))
-
-print(json.loads(resp.text))
